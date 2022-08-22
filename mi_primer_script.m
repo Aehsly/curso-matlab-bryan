d671ec4b6f131966ab7edc0d306c32f56acee2e6
Aehsly <111807270+Aehsly@users.noreply.github.com>
2022-08-22 18:04:25 -0500

    Add files via upload
    Bryan Aehsly Cárdenas Acuña 336002769

---
+++ mi_primer_script.m
@@ -0,0 +1,7 @@
+clc
+clear all
+close all
+2+5
+2.5*26
+12-45
+10/4
---
+++ script_entrada_datos.m
@@ -0,0 +1,16 @@
+clear all
+close all
+clc
+
+x=input("dame un dato x");
+y=input("dame un dato y");
+
+z= x + y;
+w=x * y;
+d= x / y;
+
+disp(["La suma de datos es ", z])
+disp(["la multiplicacion de datos es", w])
+disp(["la division de datos es ", d])
+
+
